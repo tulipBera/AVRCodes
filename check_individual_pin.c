a6e40ad3a9d9b28087128e9db102fe6ef46dbc01
tulipBera <159131630+tulipBera@users.noreply.github.com>
2024-02-06 17:20:15 +0530

    Create check_individual_pin.c

---
+++ check_individual_pin.c
@@ -0,0 +1,33 @@
+/*
+ * BCD_to_sevenSegment.cpp
+ *
+ * Created: 2/6/2024 2:26:59 PM
+ * Author : rajes
+ */ 
+
+#include <avr/io.h>
+#define F_CPU 1600000
+#include <util/delay.h>
+
+const unsigned int sevenSegment[] = {0x3F, 0x06};
+
+int main(void)
+{
+	 DDRB = 0xFF; // Set Port B as output
+	 DDRD &= ~(1 << DDD0); // Set Pin D0 as input as 0
+	 //PORTD |= (1 << PORTD1); // Enable pull-up resistor for Pin D1
+	/* Replace with your application code */
+	while (1)
+	{
+		if(PIND & (1 << PIND0)){                // check a particular pin of port D, if condition gets true execute the if statement
+			PORTB = sevenSegment[1];
+			_delay_ms(500);
+		}
+		else{
+			PORTB = sevenSegment[0];
+			_delay_ms(500);
+		}
+	}
+}
+
+
