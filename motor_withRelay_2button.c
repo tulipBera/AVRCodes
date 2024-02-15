1ebcd26f0d05549db29972ff16c219c9f9c628f7
tulipBera <159131630+tulipBera@users.noreply.github.com>
2024-02-15 11:25:55 +0530

    Update motor_withRelay_2button.c

--- motor_withRelay_2button.c
+++ motor_withRelay_2button.c
@@ -1,21 +1,26 @@
+/*
+ * motor.c
+ *
+ * Created: 2/15/2024 10:53:41 AM
+ * Author : rajes
+ */ 
+
 #include <avr/io.h>
 #define F_CPU 1600000
 #include <util/delay.h>
 
-#define Relay_PIN     PB1   
-#define BUTTON1_PIN PC0  
-#define BUTTON2_PIN PC1   
+#define Relay_PIN     PB1
+#define BUTTON1_PIN PC0
+#define BUTTON2_PIN PC1
 
 int main(void)
 {
-	DDRB = 0xFF;
-	PORTB &= ~(1 << Relay_PIN );
+	DDRB = 0x02;
 	DDRC = 0x00;
-	PORTC |= (1 << BUTTON1_PIN);
-	PORTC |= (1 << BUTTON2_PIN);
-	PORTB &= ~(1 << Relay_PIN );
+	PORTB = 0x02;
+	PORTC = 0x00;
 	while(1)
-	{		
+	{
 		if ((PINC & (1 << BUTTON1_PIN)))
 		{
 			PORTB &= ~(1 << Relay_PIN );
