bce23c2752781899f84befe36127280e3429a133
tulipBera <159131630+tulipBera@users.noreply.github.com>
2024-02-16 13:30:24 +0530

    Create motor_withInterrupt_atmega32.c

---
+++ motor_withInterrupt_atmega32.c
@@ -0,0 +1,50 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdbool.h>
+#define F_CPU 16000000UL
+#include <util/delay.h>
+
+#define BUTTON_PIN1 PD2
+#define BUTTON_PIN2 PD3
+#define RELAY_PIN PC0
+#define LED_PIN PC5
+
+volatile bool Relay_status = false;
+
+int main(void)
+{
+	DDRD = 0x00;
+	PORTD &= ~(1 << BUTTON_PIN1);
+	PORTD &= ~(1 << BUTTON_PIN2);
+	
+
+	DDRC = 0xFF;
+	PORTC |= (1 << RELAY_PIN);
+	PORTC &= ~(1 << LED_PIN); 
+
+	GICR |= (1 << INT0)|(1<<INT1);
+
+	MCUCR = 0x00;
+	sei(); 
+
+	while (1)
+	{
+		if(Relay_status == false){
+		PORTC |= (1 << LED_PIN);
+		_delay_ms(50);
+		PORTC &= ~(1 << LED_PIN);
+		_delay_ms(50);
+		}
+	}
+	return 0;
+}
+
+ISR(INT0_vect)
+{
+	PORTC &= ~(1<<RELAY_PIN);
+	Relay_status = true;
+}
+ISR(INT1_vect){
+	PORTC |= (1<<RELAY_PIN);
+	Relay_status = false;
+}
