33a0302daaef58566333ab4cb83cb8a13ad9ed71
tulipBera <159131630+tulipBera@users.noreply.github.com>
2024-02-26 11:15:22 +0530

    Update GPS_LCD_atmega32.c

--- GPS_LCD_atmega32.c
+++ GPS_LCD_atmega32.c
@@ -1,7 +1,11 @@
+//https://circuitdigest.com/microcontroller-projects/gps-module-interfacing-with-atmega16-32-avr-microcontroller
+
+
 #include <avr/io.h>
 #define F_CPU 8000000UL
 #include <util/delay.h>
 #include <avr/interrupt.h>
+#include <string.h>
 
 #define USART_BAUDRATE 9600
 
@@ -25,8 +29,9 @@ enum {
 char buf[100];
 volatile int ind = 0, flag, stringReceived = 0;
 char gpgga[] = {'$', 'G', 'P', 'G', 'G', 'A'};
-char latitude[7];
-char longitude[7];
+char latitude[10];
+char longitude[10];
+char time[7];
 
 ISR(USART_RXC_vect) {
 	char ch = UDR;
@@ -109,24 +114,24 @@ int main() {
 			ind = 0;
 			stringReceived = 0;
 			lcdwrite(0x80, 0);
-			lcdprint("Lat:");
-			for (int i = 18; i < 25; i++) {
+			//lcdprint("Lat:");
+			for (int i = 18; i < 27; i++) {
 				latitude[i - 18] = buf[i];
 				//lcdwrite(latitude[i - 18], 1);
 			}
-			char Latitude[] = {latitude[0], latitude[1], latitude[4], latitude[2], latitude[3]};
-			for (int i=0; i<5; i++)
+			char Latitude[] = {latitude[0], latitude[1], latitude[4], latitude[2], latitude[3], latitude[5], latitude[6], latitude[7], latitude[8]};
+			for (int i=0; i<9; i++)
 			{
 				lcdwrite(Latitude[i], 1);
 			}
 			lcdwrite(0xC0, 0);
-			lcdprint("Lon:");
-			for (int i = 31; i < 38; i++) {
+			//lcdprint("Lon:");
+			for (int i = 31; i < 40; i++) {
 				longitude[i - 31] = buf[i];
 				//lcdwrite(longitude[i - 31], 1);
 			}
-			char Longitude[] = {longitude[0], longitude[1], longitude[4], longitude[2], longitude[3]};
-			for (int i=0; i<5; i++)
+			char Longitude[] = {longitude[0], longitude[1], longitude[4], longitude[2], longitude[3], longitude[5], longitude[6], longitude[7], longitude[8]};
+			for (int i=0; i<9; i++)
 			{
 				lcdwrite(Longitude[i], 1);
 			}
@@ -134,7 +139,7 @@ int main() {
 			for (int i = 0; i < 100; i++) {
 				buf[i] = 0;
 			}
-			_delay_ms(1000);
+			_delay_ms(500);
 			sei();
 		}
 	}
